aafe7163a43b698a320f013709a4d16d96060c12
Doğan Can Bakır <dogancanbakir@protonmail.com>
2024-08-28 17:52:25 +0300

    allow only private TLDs

--- internal/runner/options.go
+++ internal/runner/options.go
@@ -12,6 +12,7 @@ import (
 	"github.com/projectdiscovery/goflags"
 	"github.com/projectdiscovery/gologger"
 	"github.com/projectdiscovery/tldfinder/pkg/agent"
+	"github.com/projectdiscovery/tldfinder/pkg/registry"
 	"github.com/projectdiscovery/tldfinder/pkg/resolve"
 	"github.com/projectdiscovery/tldfinder/pkg/source"
 	fileutil "github.com/projectdiscovery/utils/file"
@@ -19,6 +20,7 @@ import (
 	logutil "github.com/projectdiscovery/utils/log"
 	updateutils "github.com/projectdiscovery/utils/update"
 	"github.com/weppos/publicsuffix-go/publicsuffix"
+	"golang.org/x/exp/slices"
 )
 
 var (
@@ -180,8 +182,6 @@ func ParseOptions() *Options {
 		os.Exit(0)
 	}
 
-	options.preProcessOptions()
-
 	if !options.Silent {
 		showBanner()
 	}
@@ -201,6 +201,8 @@ func ParseOptions() *Options {
 		listSources(options)
 		os.Exit(0)
 	}
+
+	options.preProcessOptions()
 
 	// Validate the options passed by the user and if any
 	// invalid options have been used, exit.
@@ -258,17 +260,24 @@ func (options *Options) preProcessOptions() {
 	for i, query := range options.Domain {
 		query, _ = sanitize(query)
 		options.Domain[i] = options.parseQuery(query)
-
 	}
 }
 
 func (options *Options) parseQuery(query string) string {
 	if options.DiscoveryMode == source.DNSMode {
+		var tld string
 		if parsed, err := publicsuffix.Parse(query); err == nil {
-			query = parsed.TLD
+			tld = parsed.TLD
 		} else {
-			query = strings.Trim(query, ".")
+			tld = strings.Trim(query, ".")
 		}
+		if slices.Contains(registry.PrivateTLDs, tld) {
+			query = tld
+		} else {
+			gologger.Warning().Msgf("Skipping \"%s\": only private TLDs are allowed (e.g., .google)\n", query)
+			query = ""
+		}
+
 	} else if options.DiscoveryMode == source.TLDMode {
 		if parsed, err := publicsuffix.Parse(query); err == nil {
 			query = parsed.SLD
